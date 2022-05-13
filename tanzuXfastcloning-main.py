3abbc0ff8ba16eaf126cbc299dea863c7fc99be3
T A N Z U   .X.   SHI ZU KA <105282928+tanzu-M5R@users.noreply.github.com>
2022-05-13 13:01:23 +0600

    Add files via upload

---
+++ tanzuXfastcloning-main.py
@@ -0,0 +1,62 @@
+import os, time
+
+
+logo=''''
+
+\033[1;31;40m
+╔═════════════════════════════╗
+ㅤ╭━━━━╮╭━━━╮╭━╮╱╭╮╭━━━━╮╭╮╱╭╮
+ㅤ┃╭╮╭╮┃┃╭━╮┃┃┃╰╮┃┃╰━━╮━┃┃┃╱┃┃
+ㅤ╰╯┃┃╰╯┃┃╱┃┃┃╭╮╰╯┃╱╱╭╯╭╯┃┃╱┃┃
+ㅤ╱╱┃┃╱╱┃╰━╯┃┃┃╰╮┃┃╱╭╯╭╯╱┃┃╱┃┃
+ㅤ╱╱┃┃╱╱┃╭━╮┃┃┃╱┃┃┃╭╯━╰━╮┃╰━╯┃
+ㅤ╱╱╰╯╱╱╰╯╱╰╯╰╯╱╰━╯╰━━━━╯╰━━━╯
+
+ㅤㅤㅤㅤ┏━┓┏━┳━━━┳━━━┓
+ㅤㅤㅤㅤ┃┃┗┛┃┃┏━━┫┏━┓┃
+ㅤㅤㅤㅤ┃┏┓┏┓┃┗━━┫┗━┛┃
+ㅤㅤㅤㅤ┃┃┃┃┃┣━━┓┃┏┓┏┛
+ㅤㅤㅤㅤ┃┃┃┃┃┣━━┛┃┃┃┗┓
+ㅤㅤㅤㅤ┗┛┗┛┗┻━━━┻┛┗━┛
+╚═════════════════════════════╝
+\033[1;33;40m┏━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ BD All Sim Id cloning ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━┛                                                
+\033[1;33;40m--------------------------------------------------
+  \033[1;36;40mAuthor     \033[1;33;40m: \033[0;37;40mAarohi Islam Tanzu
+  \033[1;36;40mGitHub     \033[1;33;40m: \033[0;37;40mhttps://github.com/tanzu-M5R
+  \033[1;36;40mFacebook   \033[1;33;40m: \033[0;37;40mhttps://m.facebook.com/tanzu0official
+  \033[1;36;40mTelegram     \033[1;33;40m: \033[0;37;40mt.me/tanzu0official
+\033[1;33;40m--------------------------------------------------
+                                '''
+os.system('clear')
+print (logo)
+
+print('\n\t \033[1;31;40m[\033[1;33;40m1\033[1;31;40m] \033[1;33;40m6 Digit Cloning \n\t \033[1;31;40m[\033[1;33;40m2\033[1;31;40m] \033[1;33;40m7 Digit Cloning \n\t \033[1;31;40m[\033[1;33;40m3\033[1;31;40m] \033[1;33;40m8 Digit Cloning \n\t \033[1;31;40m[\033[1;33;40m4\033[1;31;40m] \033[1;33;40m11 digit Cloning \n\t \033[1;31;40m[\033[1;33;40m5\033[1;31;40m] \033[1;33;40mFollow On Facebook \n\t \033[1;31;40m[\033[1;33;40m6\033[1;31;40m]\033[1;33;40m Github \n\t \033[1;31;40m[\033[1;33;40m7\033[1;31;40m] \033[1;33;40mExit')
+
+mmm = input('\n \n\t[?]  Enter Select Option :\033[1;31;40m ')
+
+#mmm = int(mmm)
+
+if mmm == '1':
+	os.system('xdg-open https://github.com/tanzu-M5R')
+	os.system('python2 6digit.py')
+elif mmm == '2':
+	os.system('python2 7digit.py')
+elif mmm == '3':
+	os.system('python2 8digit.py')
+elif mmm == '4':
+	os.system('python2 11digit.py')
+elif mmm == '5':
+	os.system('xdg-open https://www.facebook.com/tanzu0official')
+	os.system('python main.py')
+elif mmm == '6':
+	os.system('xdg-open https://github.com/tanzu-M5R')
+	os.system('python main.py')
+elif mmm == '7':
+	os.system('exit')
+
+else:
+	print('\033[1;31;40mSelect The the correct option')
+	time.sleep(2)
+	os.system('python main.py')
